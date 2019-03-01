1f93bab43173b0fe9ef93d74758240dd65b92730
vicbalcazar <victorbalcazar3@hotmail.com>
2019-03-01 10:19:41 -0800

    Created initial components for webapp. List: Logo, Rank, ImageLinkForm, Navigation. Set up background.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react';
-import Navigation from './Components/Navigation';
+import Navigation from './Components/Navigation/Navigation';
+import Logo from './Components/Logo/Logo';
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import Rank from './Components/Rank/Rank';
+import Particles from 'react-particles-js';
 import './App.css';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 200,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles 
+          className='particles'
+          params={particlesOptions}
+        />
         <Navigation />
-        {/* <Logo />
+        <Logo />
+        <Rank />
         <ImageLinkForm />
-        <FaceRecognition /> */}
+        {/* <FaceRecognition /> */}
       </div>
     );
   }
