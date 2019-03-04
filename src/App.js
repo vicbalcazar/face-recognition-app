736b3c2c1ff79f11e98150ce62d91788cc9e4859
vicbalcazar <victorbalcazar3@hotmail.com>
2019-03-04 09:30:49 -0800

    Face Recognition now makes a bounding box on a single face in a given picture. Modified and refactored App.js as well as FaceRecognition.js. FaceRecognition Component now takes the bounding percentages of the recognized face given by the Clarifai API as properties.

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ const particlesOptions = {
   }
 };
 
+
 class App extends Component {
   //define Constructor
   constructor() {
@@ -31,7 +32,29 @@ class App extends Component {
     this.state = {
       input: '',
       imageUrl: '',
+      box:{},
     };
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('input-image');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    // console.log(data.outputs[0].data.regions[0].region_info.bounding_box);
+    // console.log(width, height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    };
+    
+  }  
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState( {box: box } ) 
   }
 
   onInputChange = (e) => {
@@ -41,18 +64,17 @@ class App extends Component {
   onBtnSubmit = () => {
     this.setState({imageUrl: this.state.input});
 
-    app.models.predict(
-      Clarifai.FACE_DETECT_MODEL, 
-      this.state.input)
-        .then(
-          function(response) {
-            // do something with response
-            console.log(response.outputs[0].data.regions[0].region_info);
-          },
-          function(err) {
-            // there was an error
-          }
-        );
+    app.models
+        .predict(
+          Clarifai.FACE_DETECT_MODEL, 
+          this.state.input)
+        .then( (response) => {
+                // do something with response
+                this.displayFaceBox(this.calculateFaceLocation(response));
+                // console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+              }
+        )
+        .catch(err => console.log(err));
   }
 
   render() {
@@ -70,6 +92,7 @@ class App extends Component {
           onBtnSubmit={this.onBtnSubmit}
         />
         <FaceRecognition 
+          box={this.state.box}
           imageUrl={this.state.imageUrl}
         />
       </div>
